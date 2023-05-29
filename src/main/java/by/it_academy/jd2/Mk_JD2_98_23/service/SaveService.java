1c52b73b65d1ef3842ef28b51edb5e9de428c327
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-05-29 22:04:50 +0300

    refactor: ISaveService, ICRUDService, SaveService

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/SaveService.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/SaveService.java
@@ -1,6 +1,15 @@
 package by.it_academy.jd2.Mk_JD2_98_23.service;
 
+import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateCreateDTO;
+import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateDTO;
 import by.it_academy.jd2.Mk_JD2_98_23.service.api.ISaveService;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SaveService implements ISaveService {
+    @Override
+    public List<RateDTO> save(RateCreateDTO item) {
+        return new ArrayList<>();
+    }
 }
--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/api/ICRUDService.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/api/ICRUDService.java
@@ -3,9 +3,5 @@ package by.it_academy.jd2.Mk_JD2_98_23.service.api;
 import java.util.List;
 
 public interface ICRUDService<T, S> {
-        List<T> get();
-
-        T get(int id);
-
-        T save (S item);
+        List<T> save (S item);
 }
