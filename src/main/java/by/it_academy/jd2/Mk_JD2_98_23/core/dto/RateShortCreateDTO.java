8d3b19e59a5fccd87e03254ac7fcd4c499e5cef8
NickSuzdalev <Nick.Suzdalev@gmail.com>
2023-05-29 20:43:59 +0300

    add: RateShortCreateDTODTO

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/core/dto/RateShortCreateDTO.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/core/dto/RateShortCreateDTO.java
@@ -2,22 +2,16 @@ package by.it_academy.jd2.Mk_JD2_98_23.core.dto;
 
 import java.time.LocalDateTime;
 
-public class RateShortDTO {
+public class RateShortCreateDTO {
 
-    private int id;
     private int Cur_ID;
     private LocalDateTime Date;
     private double Cur_OfficialRate;
 
-    public RateShortDTO() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
+    public RateShortCreateDTO(int cur_ID, LocalDateTime date, double cur_OfficialRate) {
+        Cur_ID = cur_ID;
+        Date = date;
+        Cur_OfficialRate = cur_OfficialRate;
     }
 
     public int getCur_ID() {
