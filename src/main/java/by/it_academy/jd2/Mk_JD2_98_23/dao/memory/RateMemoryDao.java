161c472f0b877bb138fd7d72aef2f2d3524a1a62
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-05-29 21:07:34 +0300

    refactor: RateMemoryDao

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/dao/memory/RateMemoryDao.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/dao/memory/RateMemoryDao.java
@@ -1,4 +1,27 @@
 package by.it_academy.jd2.Mk_JD2_98_23.dao.memory;
 
-public class RateMemoryDao {
+import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateDTO;
+import by.it_academy.jd2.Mk_JD2_98_23.dao.api.IRateDao;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class RateMemoryDao implements IRateDao {
+    private final Map<Integer, RateDTO> rateDTOMap = new ConcurrentHashMap<>();
+
+    @Override
+    public List<RateDTO> get() {
+        return null;
+    }
+
+    @Override
+    public RateDTO get(int id) {
+        return null;
+    }
+
+    @Override
+    public RateDTO save(RateDTO item) {
+        return null;
+    }
 }
