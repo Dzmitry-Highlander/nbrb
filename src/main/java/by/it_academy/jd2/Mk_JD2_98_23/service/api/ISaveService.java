4383abde740951a0bbe3f14ccea6574678d2ac4b
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-05-29 21:58:02 +0300

    feat: ISaveService

---
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/api/ISaveService.java
@@ -0,0 +1,8 @@
+package by.it_academy.jd2.Mk_JD2_98_23.service.api;
+
+import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateCreateDTO;
+import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateDTO;
+
+public interface ISaveService extends ICRUDService<RateDTO, RateCreateDTO> {
+
+}
