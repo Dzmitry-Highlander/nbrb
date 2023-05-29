e7f182f0b620fa3853d0dc1206716444f21ce6e3
NickSuzdalev <Nick.Suzdalev@gmail.com>
2023-05-30 00:48:53 +0300

    refactoting

---
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/dao/memory/factory/RateShortDaoFactory.java
@@ -0,0 +1,24 @@
+package by.it_academy.jd2.Mk_JD2_98_23.dao.memory.factory;
+
+import by.it_academy.jd2.Mk_JD2_98_23.dao.api.IRateShortDao;
+import by.it_academy.jd2.Mk_JD2_98_23.dao.memory.RateShortMemoryDao;
+
+public class RateShortDaoFactory {
+
+    private static volatile IRateShortDao instance;
+
+    private RateShortDaoFactory() {
+    }
+
+    public static IRateShortDao getInstance() {
+        if (instance == null) {
+            synchronized (RateShortDaoFactory.class) {
+                if (instance == null) {
+                    instance = new RateShortMemoryDao();
+                }
+            }
+
+        }
+        return instance;
+    }
+}
