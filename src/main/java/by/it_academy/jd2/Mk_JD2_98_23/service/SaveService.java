a33a1d04d232b6e93d97b6d244942ffe82774ef4
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-05-29 22:06:52 +0300

    refactor: SaveService

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/SaveService.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/SaveService.java
@@ -2,12 +2,19 @@ package by.it_academy.jd2.Mk_JD2_98_23.service;
 
 import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateCreateDTO;
 import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateDTO;
+import by.it_academy.jd2.Mk_JD2_98_23.dao.api.IRateDao;
 import by.it_academy.jd2.Mk_JD2_98_23.service.api.ISaveService;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class SaveService implements ISaveService {
+    private final IRateDao rateDao;
+
+    public SaveService(IRateDao rateDao) {
+        this.rateDao = rateDao;
+    }
+
     @Override
     public List<RateDTO> save(RateCreateDTO item) {
         return new ArrayList<>();
