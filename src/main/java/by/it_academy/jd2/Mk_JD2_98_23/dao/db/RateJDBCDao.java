7140357d0bbff23aee6fbf8fc12cae7b8346bac1
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-05-30 11:20:23 +0300

    feat: RateJDBCDao

---
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/dao/db/RateJDBCDao.java
@@ -0,0 +1,23 @@
+package by.it_academy.jd2.Mk_JD2_98_23.dao.db;
+
+import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateDTO;
+import by.it_academy.jd2.Mk_JD2_98_23.dao.api.IRateDao;
+
+import java.util.List;
+
+public class RateJDBCDao implements IRateDao {
+    @Override
+    public List<RateDTO> get() {
+        return null;
+    }
+
+    @Override
+    public RateDTO get(int id) {
+        return null;
+    }
+
+    @Override
+    public RateDTO save(RateDTO item) {
+        return null;
+    }
+}
