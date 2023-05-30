b9a763eb35afd315c9a85eba7d09be14010f8c03
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-05-30 11:17:58 +0300

    feat: DatabaseConnectionFactory

---
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/dao/db/ds/DatabaseConnectionFactory.java
@@ -0,0 +1,25 @@
+package by.it_academy.jd2.Mk_JD2_98_23.dao.db.ds;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseConnectionFactory {
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Ошибка, драйвер для базы не найден", e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        String url = "jdbc:postgresql://192.168.1.100:5432/rb-nb";
+        Properties props = new Properties();
+        props.setProperty("user", "postgres");
+        props.setProperty("password", "postgres");
+
+        return DriverManager.getConnection(url, props);
+    }
+}
