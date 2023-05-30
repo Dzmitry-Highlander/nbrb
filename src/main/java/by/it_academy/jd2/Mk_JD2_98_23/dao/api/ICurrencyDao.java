5e183d1658a06eb516ff134d4cf585d0b64240ad
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-05-30 18:15:55 +0300

    refactor: many code

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/dao/api/ICurrencyDao.java
+++
@@ -1,6 +0,0 @@
-package by.it_academy.jd2.Mk_JD2_98_23.dao.api;
-
-import by.it_academy.jd2.Mk_JD2_98_23.core.dto.CurrencyDTO;
-
-public interface ICurrencyDao extends ICRUDDao <CurrencyDTO>{
-}
--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/dao/memory/factory/RateDaoFactory.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/dao/memory/factory/RateDaoFactory.java
@@ -1,8 +1,6 @@
 package by.it_academy.jd2.Mk_JD2_98_23.dao.memory.factory;
 
-import by.it_academy.jd2.Mk_JD2_98_23.dao.api.ICurrencyDao;
 import by.it_academy.jd2.Mk_JD2_98_23.dao.api.IRateDao;
-import by.it_academy.jd2.Mk_JD2_98_23.dao.memory.CurrencyMemoryDao;
 import by.it_academy.jd2.Mk_JD2_98_23.dao.memory.RateMemoryDao;
 
 public class RateDaoFactory {
