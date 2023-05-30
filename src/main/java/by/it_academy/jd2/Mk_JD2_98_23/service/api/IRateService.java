5ab620fb963c820bf7062e0f2b5de9b097464aa6
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-05-30 22:41:15 +0300

    add ICRUDService.java     ICurrencyService.java     IRateService.java     IRateShortService.java

---
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/api/IRateService.java
@@ -0,0 +1,7 @@
+package by.it_academy.jd2.Mk_JD2_98_23.service.api;
+
+import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateCreateDTO;
+import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateDTO;
+
+public interface IRateService  extends ICRUDService<RateDTO, RateCreateDTO>{
+}
---
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/api/IRateShortService.java
@@ -0,0 +1,7 @@
+package by.it_academy.jd2.Mk_JD2_98_23.service.api;
+
+import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateShortCreateDTO;
+import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateShortDTO;
+
+public interface IRateShortService  extends ICRUDService<RateShortDTO, RateShortCreateDTO>{
+}
