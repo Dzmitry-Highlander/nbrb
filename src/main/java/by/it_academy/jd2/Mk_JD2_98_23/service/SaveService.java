85c77681201c7b13f49e64bd512088800d3483b3
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-05-30 22:08:08 +0300

    refactor: RateDTO, RateJDBCDao, RateMemoryDao, SaveService, SaveServlet

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/SaveService.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/SaveService.java
@@ -1,6 +1,5 @@
 package by.it_academy.jd2.Mk_JD2_98_23.service;
 
-import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateCreateDTO;
 import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateDTO;
 import by.it_academy.jd2.Mk_JD2_98_23.dao.api.IRateDao;
 import by.it_academy.jd2.Mk_JD2_98_23.service.api.ISaveService;
@@ -25,14 +24,14 @@ public class SaveService implements ISaveService {
     }
 
     @Override
-    public RateDTO save(RateCreateDTO item) {
+    public RateDTO save(RateDTO item) {
         RateDTO dto = new RateDTO();
 
         dto.setCur_ID(item.getCur_ID());
         dto.setDate(item.getDate());
         dto.setCur_Abbreviation(item.getCur_Abbreviation());
-        dto.setCurScale(item.getCur_Scale());
-        dto.setCurName(item.getCur_Name());
+        dto.setCurScale(item.getCurScale());
+        dto.setCurName(item.getCurName());
         dto.setCur_OfficialRate(item.getCur_OfficialRate());
 
         return rateDao.save(dto);
--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/api/ICRUDService.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/api/ICRUDService.java
@@ -1,11 +1,13 @@
 package by.it_academy.jd2.Mk_JD2_98_23.service.api;
 
+import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateDTO;
+
 import java.util.List;
 
-public interface ICRUDService<T, S> {
+public interface ICRUDService<T> {
         List<T> get();
 
         T get(int id);
 
-        T save(S item);
+        T save(RateDTO item);
 }
