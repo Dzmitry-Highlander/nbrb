e5f7ecf9310dce1bcd6d7ef3f3b88e746259a1e7
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-05-31 23:27:52 +0300

    ref RateService.java

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/RateService.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/RateService.java
@@ -5,6 +5,7 @@ import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateDTO;
 import by.it_academy.jd2.Mk_JD2_98_23.dao.api.IRateDao;
 import by.it_academy.jd2.Mk_JD2_98_23.service.api.IRateService;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class RateService implements IRateService {
@@ -27,15 +28,13 @@ public class RateService implements IRateService {
     }
 
     @Override
-    public RateDTO save(RateCreateDTO item) {
-        RateDTO dto = new RateDTO();
-        dto.setCur_ID(item.getCur_ID());
-        dto.setDate(item.getDate());
-        dto.setCur_Abbreviation(item.getCur_Abbreviation());
-        dto.setCur_Scale(item.getCur_Scale());
-        dto.setCur_Name(item.getCur_Name());
-        dto.setCur_OfficialRate(item.getCur_OfficialRate());
-
-        return rateDao.save(dto);
+    public void save(RateCreateDTO item) {
+     rateDao.save(item);
     }
+
+    @Override
+    public boolean checkPeriod(LocalDate from, LocalDate to) {
+        return false;
+    }
+
 }
