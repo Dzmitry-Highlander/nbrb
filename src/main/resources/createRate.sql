0cb48fcd8f9489937d39f5f2fde4c15328827c60
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-05-31 21:26:04 +0300

    refactor: createRate.sql

--- src/main/resources/createRate.sql
+++ src/main/resources/createRate.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS app.rate
     rate_id bigint NOT NULL,
     cur_id bigint NOT NULL,
     date timestamp without time zone NOT NULL,
-    cur_officialrate numeric NOT NULL,
+    cur_official_rate numeric NOT NULL,
     CONSTRAINT rate_pkey PRIMARY KEY (rate_id),
     CONSTRAINT rate_cur_id_fkey FOREIGN KEY (cur_id)
     REFERENCES app.currency (cur_id) MATCH SIMPLE
