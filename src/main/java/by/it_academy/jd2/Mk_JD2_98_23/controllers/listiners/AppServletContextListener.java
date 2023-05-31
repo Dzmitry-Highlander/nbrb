1739b6ce3950f5d845ff73b09e075f781d730086
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-05-31 14:45:39 +0300

    ref AppServletContextListener.java

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/controllers/listiners/AppServletContextListener.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/controllers/listiners/AppServletContextListener.java
@@ -1,7 +1,8 @@
 package by.it_academy.jd2.Mk_JD2_98_23.controllers.listiners;
 
-
 import by.it_academy.jd2.Mk_JD2_98_23.core.dto.CurrencyCreateDTO;
+import by.it_academy.jd2.Mk_JD2_98_23.dao.db.ds.DatabaseConnectionFactory;
+import by.it_academy.jd2.Mk_JD2_98_23.dao.exceptions.AccessDataException;
 import by.it_academy.jd2.Mk_JD2_98_23.dao.exceptions.DataInsertionErrorException;
 import by.it_academy.jd2.Mk_JD2_98_23.service.api.ICurrencyService;
 import by.it_academy.jd2.Mk_JD2_98_23.service.factory.CurrencyServiceFactory;
@@ -9,14 +10,16 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
-import jakarta.servlet.annotation.WebListener;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
-@WebListener
 public class AppServletContextListener implements ServletContextListener {
 
     private final ICurrencyService currencyService;
@@ -30,6 +33,18 @@ public class AppServletContextListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
+        try (Connection conn = DatabaseConnectionFactory.getConnection();
+             Statement st = conn.createStatement()) {
+            ResultSet rs = conn.getMetaData().getTables("", "app", "WEEKENDS", null);
+            if (!rs.next()) {
+                String sqlScript = new String(AppServletContextListener.class.getClassLoader().getResourceAsStream("data.sql").readAllBytes());
+                st.executeUpdate(sqlScript);
+            }
+
+        } catch (SQLException | IOException e) {
+            throw new AccessDataException("Ошибка загрузки данных из data.sql", e);
+        }
+
         int count = this.currencyService.getCount();
         if (count == 0) {
             try {
