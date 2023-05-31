f2592e5241a513213fa1e4ab4bba4240546aff0e
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-05-31 23:24:15 +0300

    refactor: RateServlet

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/servlets/RateServlet.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/servlets/RateServlet.java
@@ -1,8 +1,6 @@
 package by.it_academy.jd2.Mk_JD2_98_23.servlets;
 
 import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateCreateDTO;
-import by.it_academy.jd2.Mk_JD2_98_23.dao.api.IRateDao;
-import by.it_academy.jd2.Mk_JD2_98_23.dao.db.factory.RateDaoFactory;
 import by.it_academy.jd2.Mk_JD2_98_23.service.api.IRateService;
 import by.it_academy.jd2.Mk_JD2_98_23.service.factory.RateServiceFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
