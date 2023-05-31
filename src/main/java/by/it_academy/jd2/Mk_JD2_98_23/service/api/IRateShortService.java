155898bc4dbb9a1cb4c44253b9f0a011590a7fb2
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-05-31 15:39:35 +0300

    delete: unnecessary classes

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/api/IRateShortService.java
+++
@@ -1,7 +0,0 @@
-package by.it_academy.jd2.Mk_JD2_98_23.service.api;
-
-import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateShortCreateDTO;
-import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateShortDTO;
-
-public interface IRateShortService  extends ICRUDService<RateShortDTO, RateShortCreateDTO>{
-}
--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/factory/RateShortServiceFactory.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/factory/RateShortServiceFactory.java
@@ -1,7 +1,5 @@
 package by.it_academy.jd2.Mk_JD2_98_23.service.factory;
 
-import by.it_academy.jd2.Mk_JD2_98_23.dao.memory.factory.RateShortDaoFactory;
-import by.it_academy.jd2.Mk_JD2_98_23.service.RateShortService;
 import by.it_academy.jd2.Mk_JD2_98_23.service.api.IRateShortService;
 
 public class RateShortServiceFactory {
