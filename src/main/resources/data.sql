6ffe1ef754f9a3b606fba9fe487b8dcf3a4de139
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-05-31 14:46:33 +0300

    ref data.sql + create table curr and rate

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,47 @@
+DROP TABLE IF EXISTS app.CURRENCY cascade;
+
+CREATE TABLE IF NOT EXISTS app.currency
+(
+    cur_id bigint NOT NULL,
+    cur_code bigint NOT NULL,
+    cur_abbreviation text COLLATE pg_catalog."default" NOT NULL,
+    cur_name text COLLATE pg_catalog."default" NOT NULL,
+    cur_name_bel text COLLATE pg_catalog."default",
+    cur_name_eng text COLLATE pg_catalog."default",
+    cur_quotname text COLLATE pg_catalog."default",
+    cur_quotname_bel text COLLATE pg_catalog."default",
+    cur_quotname_eng text COLLATE pg_catalog."default",
+    cur_namemulti text COLLATE pg_catalog."default",
+    cur_name_belmulti text COLLATE pg_catalog."default",
+    cur_name_engmulti text COLLATE pg_catalog."default",
+    cur_scale bigint NOT NULL,
+    CONSTRAINT currency_pkey PRIMARY KEY (cur_id)
+)
+
+    TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS app.currency
+    OWNER to root;
+
+CREATE TABLE IF NOT EXISTS app.rate
+(
+    rate_id bigint NOT NULL,
+    cur_id bigint NOT NULL,
+    date timestamp without time zone NOT NULL,
+    cur_officialrate numeric NOT NULL,
+    CONSTRAINT rate_pkey PRIMARY KEY (rate_id),
+    CONSTRAINT rate_cur_id_fkey FOREIGN KEY (cur_id)
+        REFERENCES app.currency (cur_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+    TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS app.rate
+    OWNER to root;
+
+
 DROP TABLE IF EXISTS app.WEEKENDS cascade;
 
 CREATE TABLE app.WEEKENDS (
