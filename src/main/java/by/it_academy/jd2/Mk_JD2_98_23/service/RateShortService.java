480816c28da46eb7d5cfb8672924b04cd51a9280
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-05-31 23:28:56 +0300

    ref RateShortService.java

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/RateShortService.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/RateShortService.java
@@ -1,6 +1,5 @@
 package by.it_academy.jd2.Mk_JD2_98_23.service;
 
-import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateShortCreateDTO;
 import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateShortDTO;
 import by.it_academy.jd2.Mk_JD2_98_23.dao.api.IRateShortDao;
 import by.it_academy.jd2.Mk_JD2_98_23.service.api.IRateShortService;
@@ -23,15 +22,5 @@ public class RateShortService implements IRateShortService {
     @Override
     public RateShortDTO get(int id) {
         return  this.rateShortDao.get(id);
-    }
-
-    @Override
-    public RateShortDTO save(RateShortCreateDTO item) {
-        RateShortDTO dto = new RateShortDTO();
-        dto.setCur_ID(item.getCur_ID());
-        dto.setDate(item.getDate());
-        dto.setCur_OfficialRate(item.getCur_OfficialRate());
-
-        return  this.rateShortDao.save(dto);
     }
 }
