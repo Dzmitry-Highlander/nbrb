5b3e6ca5b703ebc0a233e3f4db6f9f3a9b3020e2
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-05-31 16:44:37 +0300

    refactor: many code

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/RateService.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/RateService.java
@@ -1,32 +1,31 @@
 package by.it_academy.jd2.Mk_JD2_98_23.service;
 
 import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateCreateDTO;
+import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateDTO;
 import by.it_academy.jd2.Mk_JD2_98_23.dao.api.IRateDao;
 import by.it_academy.jd2.Mk_JD2_98_23.service.api.IRateService;
 
 import java.util.List;
 
 public class RateService implements IRateService {
-
     private final IRateDao rateDao;
 
     public RateService(IRateDao rateDao) {
         this.rateDao = rateDao;
     }
 
-
     @Override
-    public List get() {
+    public List<RateDTO> get() {
         return rateDao.get();
     }
 
     @Override
-    public RateCreateDTO get(int id) {
+    public RateDTO get(int id) {
         return rateDao.get(id);
     }
 
     @Override
-    public RateCreateDTO save(RateCreateDTO item) {
+    public RateDTO save(RateCreateDTO item) {
         RateCreateDTO dto = new RateCreateDTO();
         dto.setCurID(item.getCurID());
         dto.setDate(item.getDate());
