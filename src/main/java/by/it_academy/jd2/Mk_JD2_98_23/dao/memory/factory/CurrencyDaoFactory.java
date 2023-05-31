42afbc77d55adb65868e4bc456b75e22e977621d
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-05-31 04:49:49 +0300

    ref CurrencyDaoFactory.java

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/dao/memory/factory/CurrencyDaoFactory.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/dao/memory/factory/CurrencyDaoFactory.java
@@ -1,6 +1,7 @@
 package by.it_academy.jd2.Mk_JD2_98_23.dao.memory.factory;
 
 import by.it_academy.jd2.Mk_JD2_98_23.dao.api.ICurrencyDao;
+import by.it_academy.jd2.Mk_JD2_98_23.dao.db.CurrencyJDBCDao;
 import by.it_academy.jd2.Mk_JD2_98_23.dao.memory.CurrencyMemoryDao;
 
 public class CurrencyDaoFactory {
@@ -14,7 +15,7 @@ public class CurrencyDaoFactory {
         if (instance == null)  {
             synchronized (CurrencyDaoFactory.class) {
                 if (instance == null) {
-                    instance = new CurrencyMemoryDao();
+                    instance = new CurrencyJDBCDao();
                 }
             }
 
