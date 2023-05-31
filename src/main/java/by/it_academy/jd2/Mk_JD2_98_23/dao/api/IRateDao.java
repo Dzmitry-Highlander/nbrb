ee81e76f40404d55f9802461f7d675e483932203
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-05-31 23:25:39 +0300

    ref IRateDao.java

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/dao/api/IRateDao.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/dao/api/IRateDao.java
@@ -1,6 +1,13 @@
 package by.it_academy.jd2.Mk_JD2_98_23.dao.api;
 
+import by.it_academy.jd2.Mk_JD2_98_23.core.dto.CurrencyCreateDTO;
+import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateCreateDTO;
 import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateDTO;
 
+import java.time.LocalDate;
+
 public interface IRateDao extends ICRUDDao<RateDTO> {
+    boolean checkPeriod(LocalDate from, LocalDate to);
+
+    void save(RateCreateDTO item);
 }
