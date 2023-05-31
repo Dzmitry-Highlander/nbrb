3929eb422a80d05d73aa67ea5910f46279c1bab6
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-05-31 23:26:04 +0300

    ref IRateService.java

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/api/IRateService.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/api/IRateService.java
@@ -3,5 +3,10 @@ package by.it_academy.jd2.Mk_JD2_98_23.service.api;
 import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateCreateDTO;
 import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateDTO;
 
-public interface IRateService  extends ICRUDService<RateDTO, RateCreateDTO>{
+import java.time.LocalDate;
+
+public interface IRateService extends ICRUDService<RateDTO, RateCreateDTO> {
+    boolean checkPeriod(LocalDate from, LocalDate to);
+
+    void save(RateCreateDTO item);
 }
