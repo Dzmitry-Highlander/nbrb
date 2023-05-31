a3cd42ef297118f78ef13a9aaaf51666a97009ad
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-05-31 04:56:09 +0300

    ref ICurrencyDao.java     ICurrencyService.java

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/api/ICurrencyService.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/api/ICurrencyService.java
@@ -4,4 +4,5 @@ import by.it_academy.jd2.Mk_JD2_98_23.core.dto.CurrencyCreateDTO;
 import by.it_academy.jd2.Mk_JD2_98_23.core.dto.CurrencyDTO;
 
 public interface ICurrencyService extends ICRUDService<CurrencyDTO, CurrencyCreateDTO> {
+    void uploadData (CurrencyCreateDTO item);
 }
