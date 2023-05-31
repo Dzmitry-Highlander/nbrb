24d5a7c92bb4fbea1fc8cf6e45de82a7cff64edf
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-05-31 15:50:14 +0300

    refactor: CurrencyServletTest

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/controllers/web/CurrencyServletTest.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/controllers/web/CurrencyServletTest.java
@@ -14,10 +14,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-
 @WebServlet(urlPatterns = "/test")
 public class CurrencyServletTest extends HttpServlet {
-
     private final ICurrencyService currencyService;
     private final ObjectMapper objectMapper;
 
@@ -32,15 +30,10 @@ public class CurrencyServletTest extends HttpServlet {
         req.setCharacterEncoding("UTF-8");
         resp.setContentType("application/json; charset=UTF-8");
 
-
-
         List<CurrencyDTO> list = currencyService.get();
-
-
 
         for (CurrencyDTO currencyCreateDTO : list) {
             resp.getWriter().write(currencyCreateDTO.getCurName() + "\n");
-
         }
 
         resp.getWriter().write(currencyService.get(456).getCurName() + "\n" + "TEEEEST ONLY ID");
