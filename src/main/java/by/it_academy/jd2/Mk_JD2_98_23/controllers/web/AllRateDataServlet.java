215c773021e0f7b9b7b1c0c232c6c449208e08d5
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-06-01 15:28:42 +0300

    add AllRateDataServlet.java

---
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/controllers/web/AllRateDataServlet.java
@@ -0,0 +1,50 @@
+package by.it_academy.jd2.Mk_JD2_98_23.controllers.web;
+
+import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateCreateDTO;
+import by.it_academy.jd2.Mk_JD2_98_23.service.api.ICurrencyService;
+import by.it_academy.jd2.Mk_JD2_98_23.service.api.IRateService;
+import by.it_academy.jd2.Mk_JD2_98_23.service.factory.CurrencyServiceFactory;
+import by.it_academy.jd2.Mk_JD2_98_23.service.factory.ObjectMapperFactory;
+import by.it_academy.jd2.Mk_JD2_98_23.service.factory.RateServiceFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Objects;
+
+
+@WebServlet(urlPatterns = "/all-rate")
+public class AllRateDataServlet extends HttpServlet {
+    private static final String CURRENCY = "сur_abbreviation";
+
+    private final IRateService rateService;
+    private final ICurrencyService currencyService;
+    private final ObjectMapper objectMapper;
+
+    public AllRateDataServlet() {
+        this.rateService = RateServiceFactory.getInstance();
+        this.currencyService = CurrencyServiceFactory.getInstance();
+        this.objectMapper = ObjectMapperFactory.getInstance();
+        this.objectMapper.findAndRegisterModules();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("application/json; charset=UTF-8");
+
+        String currency = req.getParameter(CURRENCY);
+        PrintWriter writer = resp.getWriter();
+
+        if (!Objects.equals(currency, "" )) {
+            rateService.get(currency);
+
+//            writer.write(rateCreateDTOS.toString());
+        }
+    }
+}
