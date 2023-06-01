04c823b2f8a06c5cb659176cc970610fe8f8c57a
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-06-01 20:55:40 +0300

    ref IRateDao.java

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/dao/api/IRateDao.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/dao/api/IRateDao.java
@@ -14,4 +14,6 @@ public interface IRateDao extends ICRUDDao<RateCreateDTO> {
     boolean checkRateData(RateCreateDTO item);
 
     List<RateDTO> get(String curAbbreviation);
+
+    double getAverageCurrency(LocalDate date);
 }
