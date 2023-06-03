53fb641d9198fc0c25577b7f187c096d1a7490f7
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-06-03 19:12:05 +0300

    del   AppServletContextListener.java  del   createRate.sql  add   currencyDDL.sql  add   currencyDML.sql  del   dataSources.xml  del   dataWeekend.sql  add   rateDDL.sql  ref   web.xml  add   weekendsDDL.sql  add   weekwndDML.sql

--- src/main/resources/weekwndDML.sql
+++ src/main/resources/weekwndDML.sql
@@ -1,10 +1,3 @@
-DROP TABLE IF EXISTS app.WEEKENDS cascade;
-
-CREATE TABLE app.WEEKENDS (
-                              CALENDAR_DATE DATE NULL,
-                              IS_DAY_OFF INTEGER  NULL,
-                              CONSTRAINT PK_WEEKEND12345687 PRIMARY KEY (CALENDAR_DATE)
-);
 
 INSERT INTO  app.WEEKENDS (CALENDAR_DATE, IS_DAY_OFF)
 VALUES
