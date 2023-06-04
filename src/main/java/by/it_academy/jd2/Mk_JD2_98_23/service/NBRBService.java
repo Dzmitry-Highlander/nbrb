1a33018bbce5fe6182f56dbf9f1ee4e216696626
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-06-04 14:19:17 +0300

    ref  NBRBService.java

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/NBRBService.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/NBRBService.java
@@ -1,7 +1,9 @@
 package by.it_academy.jd2.Mk_JD2_98_23.service;
 
 
+import by.it_academy.jd2.Mk_JD2_98_23.core.dto.CurrencyCreateDTO;
 import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RateCreateDTO;
+import by.it_academy.jd2.Mk_JD2_98_23.core.dto.CurrencyRateTodayDTO;
 import by.it_academy.jd2.Mk_JD2_98_23.core.dto.RatePeriodDTO;
 import by.it_academy.jd2.Mk_JD2_98_23.service.api.IBankApiService;
 import by.it_academy.jd2.Mk_JD2_98_23.service.factory.ObjectMapperFactory;
@@ -15,11 +17,11 @@ import java.util.List;
 
 
 public class NBRBService implements IBankApiService {
-    private static final String BASE_URL = "https://www.nbrb.by/api/exrates/";
+    private static final String BASE_URL = "https://api.nbrb.by/exrates/";
 
     @Override
     public List<RateCreateDTO> getRatesFromExternalAPI(int cur, RatePeriodDTO item) {
-        String url = BASE_URL + "dynamics/" + cur + "?startdate=" + item.getStartDate() + "&enddate=" + item.getEndDate();
+        String url = BASE_URL + "rates/dynamics/" + cur + "?startdate=" + item.getStartDate() + "&enddate=" + item.getEndDate();
         List<RateCreateDTO> rateCreateDTOS;
         try {
             URL obj = new URL(url);
@@ -35,5 +37,35 @@ public class NBRBService implements IBankApiService {
             throw new RuntimeException(e);
         }
         return rateCreateDTOS;
+    }
+
+    @Override
+    public int getCurIdFromApi(String curAbb) {
+        String url = BASE_URL + "rates/" + curAbb + "?parammode=2";
+        URL obj = null;
+        CurrencyRateTodayDTO currencyRateTodayDTO = null;
+        try {
+            obj = new URL(url);
+            ObjectMapper mapper = ObjectMapperFactory.getInstance();
+            currencyRateTodayDTO = mapper.readValue(obj, CurrencyRateTodayDTO.class);
+        } catch (IOException e) {
+            throw new RuntimeException("Этот тип валюты не представлен в API", e);
+        }
+        return currencyRateTodayDTO.getCurID();
+    }
+
+    @Override
+    public CurrencyCreateDTO getSelectedCurrencyFromApi(int curId) {
+        String url = BASE_URL+ "currencies/" + curId;
+        URL obj = null;
+        CurrencyCreateDTO currencyCreateDTO = null;
+        try {
+            obj = new URL(url);
+            ObjectMapper mapper = ObjectMapperFactory.getInstance();
+            currencyCreateDTO = mapper.readValue(obj, CurrencyCreateDTO.class);
+        } catch (IOException e) {
+            throw new RuntimeException("Ошибка при получении новой валюты из API", e);
+        }
+        return currencyCreateDTO;
     }
 }
